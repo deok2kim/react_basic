f446d07c29e45e777fb3d70d8fb6e072dbb91f91
deok2 <dukgi77@naver.com>
2021-08-27 11:09:01 +0900

    11. 배열 렌더링하

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 // import Counter from "./Counter.js";
-import InputSample from "./InputSample.js";
+// import InputSample from "./InputSample.js";
+import UserList from "./UserList.js";
 
 function App() {
-  return <InputSample />;
+  return <UserList />;
 }
 
 export default App;
